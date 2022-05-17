40377f0ea26d636d7a0e5f5473ac40c1889f09b0
Bart-Zw <9007171@student.zadkine.nl>
2022-05-17 12:41:47 +0200

    push

--- app.js
+++ app.js
@@ -2,8 +2,21 @@
 let name;
 let country;
 let p = document.getElementById("output");
+let nameChange = document.getElementById("nameChange");
+let countryChange = document.getElementById("countryChange");
 
-const nameChanged = function(event){
+nameChange.addEventListener("change", function(){
+    name = event.target.value;
+    changeInput();
+
+
+})
+countryChange.addEventListener("change", function(){
+    country = event.target.value;
+    changeInput();
+})
+
+/*const nameChanged = function(event){
     //het event loggen in je console
     name = event.target.value;
     changeInput();
@@ -14,9 +27,10 @@ const nameChanged = function(event){
 const countryChange = function(event){
     country = event.target.value;
     changeInput();
-}
+}*/
 
 const changeInput = function(event){
     const text = `Je heet ${name} en je komt uit ${country}`; 
     p.textContent = text;
+
 }
